45e1dcf9218c13c7f1a6bf15c66378213f3ecce5
grmkris <kristjan.grm1@gmail.com>
2026-01-04 16:25:23 -0300

    fix migration

--- packages/db/drizzle/0011_melted_unus.sql
+++ packages/db/drizzle/0011_melted_unus.sql
@@ -1,4 +1,6 @@
 ALTER TABLE "wallet_address" ALTER COLUMN "chain_id" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "wallet_address" ADD COLUMN "chain_namespace" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "wallet_address" ADD COLUMN "chain_namespace" text;--> statement-breakpoint
+UPDATE "wallet_address" SET "chain_namespace" = 'eip155' WHERE "chain_namespace" IS NULL;--> statement-breakpoint
+ALTER TABLE "wallet_address" ALTER COLUMN "chain_namespace" SET NOT NULL;--> statement-breakpoint
 CREATE INDEX "walletAddress_address_idx" ON "wallet_address" USING btree ("address");--> statement-breakpoint
 ALTER TABLE "market" DROP COLUMN "resolution_strategy";
